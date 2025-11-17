f7b39306fd3c12cf872a37235184b4fb3df2975a
andreireporter13 <andreireporter13>
2025-11-17 22:30:38 +0200

    added new version of George Popa website

---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="count++">Clicked {{ count }} times</button>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const count = ref(0);
+</script>
+
+<style>
+button {
+  background-color: #1d4ed8;
+  color: white;
+  padding: 0.5rem 1rem;
+  border-radius: 0.25rem;
+  border: none;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #2563eb;
+}
+</style>
---
+++ src/composables/navItems.js
@@ -0,0 +1,28 @@
+
+export const navItemsRO = {
+  home: { label: "Fotograf", href: "/ro" },
+  items: [
+    { label: "Despre mine", href: "/ro/despre-mine" },
+    { label: "Contact", href: "/ro/contact" },
+    { label: "Botez", href: "/ro/botez" },
+    { label: "Cununie", href: "/ro/nunta/cununie" },
+    { label: "Trash the Dress", href: "/ro/nunta/trash-the-dress" },
+    { label: "Maternitate", href: "/ro/sedinte/maternitate" },
+    { label: "Nou-născuți", href: "/ro/sedinte/fotografie-nou-nascut" },
+    { label: "Copii", href: "/ro/sedinte/copii" },
+  ]
+};
+
+export const navItemsEN = {
+  home: { label: "Home", href: "/en" },
+  items: [
+    { label: "About Me", href: "/en/about-me" },
+    { label: "Contact", href: "/en/contact" },
+    { label: "Baptism", href: "/en/baptism" },
+    { label: "Marriage", href: "/en/wedding/marriage" },
+    { label: "Trash the Dress", href: "/en/wedding/trash-the-dress" },
+    { label: "Maternity", href: "/en/sessions/maternity" },
+    { label: "Newborn", href: "/en/sessions/newborn" },
+    { label: "Kids", href: "/en/sessions/kids" },
+  ]
+};
